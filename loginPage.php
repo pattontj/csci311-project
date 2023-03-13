4bf9bafb4f5e22b133211102ec77f5ffd5a46c99
Davon Taylor <taylord@stumail.csci.viu.ca>
2023-03-13 12:45:43 -0700

    Created non-functional login form

--- loginPage.php
+++ loginPage.php
@@ -3,17 +3,26 @@
 <head>
   <meta charset="utf-8">
   <!--Page Title-->
-  <title></title>
+  <title>Login Page</title>
   <!--Link to StyleSheet-->
   <link href="css/style.css" rel="stylesheet" />
-
+  <!--Page Icon-->
+  <link rel="icon" type="image/png" href="pathToImage">
+  
 </head>
 <body>
-	<form action="futurescriptname" method="POST">
-		<input type="text"/>
-	
-	
+	<div class="center">
+	<form action="loginHandler.php" method="POST">
+		<label for="userName">UserName:</label><br>
+		<input type="text" id="userName" name="userName" placeholder="Karl_Wheezer" size="20" required><br><br>
+		<label for="password">Password:</label><br>
+		<input type="paswword" id="password" name="password" placeholder="Enter_Password" size="20" required><br><br>
+		<label for="pfp">Select a Profile Icon:</label><br>
+		<input type="file" id="pfp" name="pfp"><br><br>
+		<input type="submit" value="Login">
+		<input type="reset" value="Reset">
 	</form>
+	</div>
 	
 	
 	
