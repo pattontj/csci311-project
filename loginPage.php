5e71e609403c866a97e2d18476a5c6fd154cf643
Davon Taylor <taylord@stumail.csci.viu.ca>
2023-03-21 13:11:00 -0700

    Began improvements on login pages and added a link to login from create account

--- loginPage.php
+++ loginPage.php
@@ -15,33 +15,34 @@ session_start();
   
 </head>
 <body>
-	<div class="center">
-	<!-- Once user selects Login they will be redirected to their home page -->
-	<form action="loginHandler.php" method="post">
+	<div class="loginCreate">
+		<div class="center">
+		<!-- Once user selects Login they will be redirected to their home page -->
+		<form action="loginHandler.php" method="post">
 
 
-     <?php
-     if ($_SESSION["failedAttempt"] || $_SESSION["failedAttempt"] == true) {
-         echo "<a>Login attempt failed</a> <br/> <br />";
-         $_SESSION["failedAttempt"] = false;
-     }
-     ?>
+		<?php
+		if ($_SESSION["failedAttempt"] || $_SESSION["failedAttempt"] == true) {
+			echo "<a>Login attempt failed</a> <br/> <br />";
+			$_SESSION["failedAttempt"] = false;
+		}
+		?>
      
-     <label for="userName">UserName:</label><br>
-		<input type="text" id="userName" name="userName" placeholder="Karl_Wheezer" size="20" required><br><br>
-		<label for="password">Password:</label><br>
-		<input type="password" id="password" name="password" placeholder="Enter_Password" size="20" required><br><br>
-     <input type="submit" value="Login">
-     <input type="reset" value="Reset">
-	</form>
-
-     <div>
-     <br />
-     <a href="./createAccountPage.php">Create account</a>
-     </div>
-     
-     </div>
+		<label for="userName">UserName:</label><br>
+			<input type="text" id="userName" name="userName" placeholder="Karl_Wheezer" size="20" required><br><br>
+			<label for="password">Password:</label><br>
+			<input type="password" id="password" name="password" placeholder="Enter_Password" size="20" required><br><br>
+		<input type="submit" value="Login">
+		<input type="reset" value="Reset">
+		</form>
 	
+		<div>
+			<br>
+			<a href="./createAccountPage.php">New User? Create a account</a>
+		</div>
+     
+		</div>
+	</div>
 	
 	
 </body>
